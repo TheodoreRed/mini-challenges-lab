d518c1bcddc842f6c650f0a3757f67ab9e819bbf
Theodore Redlon <redlontheo@gmail.com>
2023-10-18 08:19:24 -0400

    Replace datalist with radio buttons for coin selection

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ gummyBtn.addEventListener("click", () => {
 // Make Money
 // selectors & variables
 const quantityInput = document.querySelector("#quantity");
-const whichCoinInput = document.querySelector("#which-coin");
 const coinContainer = document.querySelector(".coin-container");
+let coinOptions = document.getElementsByName("coinType");
 const arrayOfCoins = document.querySelectorAll(".coin");
 const makeMoneyForm = document.querySelector(".make-money-form");
 const coinTotalDisplay = document.querySelector(".coin-total-display");
@@ -42,26 +42,32 @@ makeMoneyForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let quantity = quantityInput.value;
-  let coin = whichCoinInput.value;
+  let coin;
+
+  for (const c of coinOptions) {
+    if (c.checked) {
+      coin = c.id;
+    }
+  }
 
   for (let i = 1; i <= quantity; i++) {
     const coinNode = document.createElement("div");
     coinNode.textContent = coin;
     let coinValue = 0.0;
     switch (coin) {
-      case "Penny":
+      case "penny":
         coinNode.classList.add("coin", "penny");
         coinValue = 0.01;
         break;
-      case "Nickel":
+      case "nickel":
         coinNode.classList.add("coin", "nickel");
         coinValue = 0.05;
         break;
-      case "Dime":
+      case "dime":
         coinNode.classList.add("coin", "dime");
         coinValue = 0.1;
         break;
-      case "Quarter":
+      case "quarter":
         coinNode.classList.add("coin", "quarter");
         coinValue = 0.25;
         break;
@@ -86,7 +92,6 @@ makeMoneyForm.addEventListener("submit", (e) => {
   }
   coinTotalDisplay.textContent = `Total: $${runningTotal.toFixed(2)}`;
   quantityInput.value = null;
-  whichCoinInput.value = "";
 });
 
 // Light Bulb
